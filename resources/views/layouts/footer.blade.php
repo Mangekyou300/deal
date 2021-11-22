7e5fb4bab1e60477bcef2f2230aebb4e05a2d0af
Aurélio Silva de Jesus <aurelin95@gmail.com>
2021-11-21 21:36:49 -0300

    dding footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+    <span>&copy {{ __('Copyright DEAL - Todos os direitos reservados.') }}</span>
+    <img src="{{ asset('deal/image/logos/deal-logo-branco-cortado.png') }}" alt="Logo deal">
+</footer>
