266c07195af8b0abac8d0ff1478750d9cf60e305
Mangekyou300 <guilhermealexsander30@hotmail.com>
2021-12-07 20:38:10 -0300

    criação condicional e exclusão de habilidades

--- public/deal/js/vagas/create.js
+++ public/deal/js/vagas/create.js
@@ -1,7 +1,15 @@
 const btnAddAbility = document.getElementById('add_ability');
 const ulAbilities = document.getElementById('abilities');
 const inputHabilidades = document.getElementById('habilidades');
+const formCreateVaga = document.getElementById('form_create_vaga');
 const abilities = [];
+const dt_inicio = document.getElementById('start_date');
+
+
+const n =  new Date();
+const y = n.getFullYear();
+const m = n.getMonth() + 1;
+const d = n.getDate();
 
 btnAddAbility.addEventListener('click', function(event) {
 
@@ -12,27 +20,61 @@ btnAddAbility.addEventListener('click', function(event) {
     reloadUlAbilities();
 });
 
+formCreateVaga.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("nada não");
+});
+
 function addAbilityToArray() {
     
     const { value } = inputHabilidades;
     abilities.push(value);
 }
 
+function removeAbility(index){
+    abilities.splice(index, 1);
+    reloadUlAbilities();
+}
+
 function reloadUlAbilities() {
 
     ulAbilities.innerHTML = '';
-
+    
     if(abilities.length > 0) {
 
-        abilities.forEach(e => {
+        abilities.forEach((e, i) => {
 
             const li = document.createElement('li');
 
             li.innerHTML = `
                 <i class="fas fa-angle-double-right"></i>
                 <span>${e}</span>
-            `
+                <button class="less" onclick="removeAbility(${i})">
+                <i class="fas fa-minus-circle"></i>
+                </button>
+                `
             ulAbilities.appendChild(li);
         })
     }
+    
 }
+
+dt_inicio.value = formatDate(`${y}-${m}-${d}`); 
+
+
+function formatDate(date){
+    let [y,m,d] = date.split('-');
+    m = String(m).length != 2 ? `0${m}` : String(m);
+    d = String(d).length != 2 ? `0${d}` : String(d);
+    return `${y}-${m}-${d}`;
+}
+
+inputHabilidades.addEventListener('input', function(event){
+
+    if(event.target.value.length >= 3 ){
+    btnAddAbility.disabled = false;
+        
+    }else{
+        btnAddAbility.disabled = true;
+    }
+});
--- resources/views/deal/vagas/create.blade.php
+++ resources/views/deal/vagas/create.blade.php
@@ -8,8 +8,7 @@
 
         <div class="card default-radius">
             <div class="card-body">
-                <form action="{{ route('vagas.store') }}" method="post">
-                    @csrf
+                <form id="form_create_vaga">
                     <div class="vaga_photo_container">
                         <div class="photo-container">
                             <i class="fas fa-gavel"></i>
@@ -28,7 +27,7 @@
                             <div class="form-group">
                                 <label for="titulo">Título da vaga</label>
                                 <input type="text" class="form-control form-control-sm" name="titulo" id="titulo"
-                                    placeholder="">
+                                    placeholder="" required>
                             </div>
                         </div>
                     </div>
@@ -36,7 +35,7 @@
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="descricao">Descrição</label>
-                                <textarea class="form-control" name="descricao" id="descricao" rows="2"></textarea>
+                                <textarea class="form-control" name="descricao" id="descricao" rows="2" required></textarea>
                                 <small class="form-text text-muted">Ajude às pessoas a entender melhor sobre a sua
                                     demanda!</small>
                             </div>
@@ -54,7 +53,7 @@
                                 <div class="input-group">
                                     <input type="text" class="form-control" name="habilidades" id="habilidades" placeholder="Adicione uma habilidade" required>
                                     <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" id="add_ability" type="button">
+                                        <button class="btn btn-outline-secondary" id="add_ability" type="button" disabled="true">
                                             <i class="fas fa-plus"></i>
                                         </button>
                                     </div>
@@ -67,11 +66,36 @@
                         <ul id="abilities"></ul>
                     </div>
 
+                    <h3 class="subtitle mb-0 mt-4">Até quando estarão abertas as inscrições?</h3>
+
+                    <div class="between"></div>
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="from-group">
+                                <div class="startDate">
+                                    <label for="start_date">Data de inicio</label>
+                                    <input type="date" class="mb-5 form-control" id="start_date" name="start_date" value="" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="from-group">
+                                <div class="date">
+                                    <label for="data_de_expiracao">Data limite</label>
+                                    <input type="date" class="mb-5 form-control" name="data_de_expiracao" required>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <button type="submit" class="btn btn-submit-custom">Salvar</button>
                 </form>
             </div>
         </div>
     </div>
+
+    
 
 @endsection
 
