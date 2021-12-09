b9a02d0e32349428e63fe8c68627bcbc4ae3bba3
Mangekyou300 <guilhermealexsander30@hotmail.com>
2021-12-09 15:40:47 -0300

    add radio type_job

--- resources/views/deal/vagas/create.blade.php
+++ resources/views/deal/vagas/create.blade.php
@@ -51,7 +51,7 @@
                             <div class="form-group">
                                 <label for="habilidades">Habilidades necessárias</label>
                                 <div class="input-group">
-                                    <input type="text" class="form-control" name="habilidades" id="habilidades" placeholder="Adicione uma habilidade" required>
+                                    <input type="text" class="form-control" name="habilidades" id="habilidades" placeholder="Adicione uma habilidade">
                                     <div class="input-group-append">
                                         <button class="btn btn-outline-secondary" id="add_ability" type="button" disabled="true">
                                             <i class="fas fa-plus"></i>
@@ -72,24 +72,49 @@
 
                     <div class="row">
                         <div class="col-md-3">
-                            <div class="from-group">
+                            <div class="form-group">
                                 <div class="startDate">
                                     <label for="start_date">Data de inicio</label>
-                                    <input type="date" class="mb-5 form-control" id="start_date" name="start_date" value="" required>
+                                    <input type="date" class="form-control" id="start_date" name="start_date" value="" required>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-3">
-                            <div class="from-group">
+                            <div class="form-group">
                                 <div class="date">
                                     <label for="expire_date">Data limite</label>
-                                    <input type="date" class="mb-5 form-control" id="expire_date" name="expire_date" required>
+                                    <input type="date" class="form-control" id="expire_date" name="expire_date" required>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    
+                    <h3 class="subtitle mb-0 mt-4">Qual tipo de emprego está oferencendo?</h3>
 
-                    <button type="submit" class="btn btn-submit-custom">Salvar</button>
+                    <div class="between"></div>
+
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="radio"  class="form-check-input" id="fixed_employment" name="job_type" value="1">
+                                        Emprego Fixo
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-check">
+                                <label class="form-check-label">
+                                    <input type="radio" class="form-check-input" id="freelance" name="job_type" value="2">  
+                                    Freelance
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-submit-custom mt-4">Salvar</button>
                 </form>
             </div>
         </div>
